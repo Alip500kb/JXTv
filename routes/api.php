628e7d29b898c9deac6c8b8368cf001912fdfabd
Alip Jyki <alifbintangajayaki@gmail.com>
2026-07-01 15:27:53 +0700

    more featured database and some code changes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Auths;
 use App\Http\Controllers\tver;
+use App\Http\Controllers\user_manager;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -11,6 +12,7 @@ Route::get('/user', function (Request $request) {
 Route::post('/signup', [Auths::class, 'signup'])->middleware('throttle:6,3');
 Route::get('/login', [Auths::class, 'login'])->middleware('throttle:6,1');
 Route::get('/logout', [Auths::class, 'logout'])->middleware('auth:sanctum');
-Route::get('/log_info', [Auths::class, 'log_info'])->middleware('auth:sanctum');
+Route::get('/user_info', [user_manager::class, 'user_info'])->middleware('auth:sanctum');
 Route::get('/tv', [tver::class, 'get_all'])->middleware('throttle:36,1');
 Route::post('/tv', [tver::class, 'up_tv']);
+Route::post('/tv_rate/{$id}', [tver::class, 'tv_rate'])->middleware(['auth:sanctum','throttle:6,1']);
