32084456c9f5f1f129874668ec6d18163e5d9901
Alip Jyki <alifbintangajayaki@gmail.com>
2026-07-01 16:01:19 +0700

    featured get_all and adding recommended algorithm

--- app/Http/Controllers/tver.php
+++ app/Http/Controllers/tver.php
@@ -15,6 +15,7 @@ class tver extends Controller
         $tipe = $request->header('by', 'asc');
         $id = $request->query('id', );
         $category = $request->query('catg',);
+        $country = $request->query('country',);
         if ($id) {
             $channel = tv_list::where('id',$id)->first();
             $channel->update([
@@ -23,9 +24,24 @@ class tver extends Controller
             return response()->json();
         } elseif ($category) {
             $kats = tv_list::where('category', $category)->get();
+            $country ? $kats->where('country', $country) : $kats;
             return response()->json($kats ? $kats : 'kategori tidak ditemukan');
+        } elseif ($country) {
+            return response()->json(tv_list::where('country', $country)->get());
         }
         return response()->json(tv_list::orderBy($sorts, $tipe)->skip($halaman * 5)->take(5)->get(),200);
+    }
+
+    public function get_recommend(Request $request) {
+        $by_country = $request->header('country',);
+        $by_category = $request->header('category',);
+
+        if ($by_category) {
+            return response()->json(tv_list::where('category', $by_category)->random(10)->get());
+        } elseif($by_country) {
+            return response()->json(tv_list::where('country', $by_country)->random(10)->get());
+        }
+        return response()->json(tv_list::random(36));
     }
 
     public function up_tv(Request $request) {
